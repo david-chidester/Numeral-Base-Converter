368ec0c2d9402068d4c7b3d4c0e11074f339eef9
David Chidester <31967957+david-chidester@users.noreply.github.com>
2020-06-03 19:09:44 -0700

    Removed extra '>' char in comments

--- NumeralBaseConverter.java
+++ NumeralBaseConverter.java
@@ -1,5 +1,5 @@
 /*  <Numeral Base Converter>
-*   Copyright (C) <2019>>  <David N. Tishkoff Chidester>
+*   Copyright (C) <2019>  <David N. Tishkoff Chidester>
 *
 *   This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
