4aaf8163cd242dca2b0aa6f8e123d70344422547
David Chidester <31967957+david-chidester@users.noreply.github.com>
2020-10-22 18:18:13 -0700

    Implemented less verbose methods with bitwise not

--- TwosCompliment.java
+++ TwosCompliment.java
@@ -2,50 +2,33 @@ import java.lang.*;
 
 public class TwosCompliment {
     public static void main(String args[]){
-        String test = twosCompliment((long) (10000));
-        parseBytes(test);
+        for (int i = 0; i < 100; i++){
+            String twos = twosCompliment((long)(i));
+            String ones = onesCompliment((long)(i));
+            System.out.print("Twos Compliment of "+i+"= ");
+            parseBytes(twos);
+            System.out.print("\nOnes Compliment of "+i+"= ");
+            parseBytes(ones);
+            System.out.println("");
+        }
     }
 
     private static String onesCompliment(long input){
-        char parsedBinary[] = Long.toBinaryString(input).toCharArray();
-        char output[] = new char[64];
-        // Appending 0s if long doesn't use 64 bits
-        for (int i = 0; i < output.length - parsedBinary.length; i++)
-            output[i] = '0';
-
-        for (int i = output.length - parsedBinary.length; i < output.length; i++)
-            output[i] = parsedBinary[i - (output.length - parsedBinary.length)];
-
-        // flipping bits
-        for (int i = 0; i < output.length; i++)
-            output[i] = (output[i] == '1'? '0' : '1');
-
         StringBuilder sb = new StringBuilder();
-        sb.append(output);
-        return sb.toString();
+        sb.append(Long.toBinaryString(~input));
+        sb = sb.reverse();
+        while (sb.length() < 64)
+            sb.append('0');
+        return sb.reverse().toString();
     }
 
     private static String twosCompliment(long input){
-        char compliment[] = onesCompliment(input).toCharArray();
         StringBuilder sb = new StringBuilder();
-
-        // Incrementing by 1
-        if (compliment[compliment.length-1] == '0'){
-            compliment[compliment.length-1] = '1';
-            sb.append(compliment);
-            return sb.toString();
-        }
-        // If lowest order bit 1 then carry
-        boolean carry = true;
-        int i = compliment.length-1;
-        while (i >= 0 && carry){
-            carry = (compliment[i] == '1') && carry;
-            compliment[i] = (compliment[i] == '1'? '0' : '1');
-            // exit loop when there is nothing to carry
-            i--;
-        }
-        sb.append(compliment);
-        return sb.toString();
+        sb.append(Long.toBinaryString((~input)+1));
+        sb = sb.reverse();
+        while (sb.length() < 64)
+            sb.append('0');
+        return sb.reverse().toString();
     }
 
     // Prints out string byte by byte for testing
@@ -56,7 +39,7 @@ public class TwosCompliment {
               System.out.print(" ");
           System.out.print(output[i]);
       }
-      System.out.println("\n");
+      System.out.println("");
     }
 
 }
